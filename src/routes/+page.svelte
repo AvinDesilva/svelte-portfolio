e19d38b57ebf08215105ecd4d02f538787a8e17b
Avin deSilva <desilva.a11@gmail.com>
2024-05-12 15:50:46 -0500

    fixed error with dom not updating

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script>
+  import { onMount } from 'svelte';
   import Header from '../components/header.svelte';
   import { theme } from '../themeStore.ts';
 
-  // Subscribe to theme changes and update the CSS variables globally
-  $: {
-    document.documentElement.style.setProperty('--primary-color', $theme.primaryColor);
-    document.documentElement.style.setProperty('--secondary-color', $theme.secondaryColor);
-  }
+  onMount(() => {
+    const unsubscribe = theme.subscribe(($theme) => {
+      document.documentElement.style.setProperty('--primary-color', $theme.primaryColor);
+      document.documentElement.style.setProperty('--secondary-color', $theme.secondaryColor);
+    });
+
+    // Cleanup function to unsubscribe when the component is destroyed
+    return unsubscribe;
+  });
 </script>
 
 <style>
